04bf435ad79759764102bbf1014f944d3abbffa6
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-05-16 09:50:18 +0100

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -185,7 +185,7 @@ def login_view(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=username, password=password)
 
         if user is not None:
             login(request, user)
